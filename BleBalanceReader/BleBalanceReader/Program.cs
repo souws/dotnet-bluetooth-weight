dd6ec8c46c62110ba60bbb92b4d9682417c732a0
LeiYangGH <675686066@qq.com>
2020-11-20 23:49:09 +0800

    can get weight

---
+++ BleBalanceReader/BleBalanceReader/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Sockets;
+using System.Linq;
+namespace BleBalanceReader
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            BluetoothClient client = new BluetoothClient();
+            var devices = client.DiscoverDevices();
+            foreach (BluetoothDeviceInfo dvc in devices)
+            {
+
+                Console.WriteLine(dvc.DeviceName);
+                if (dvc.DeviceName == "JDY-33-SPP")
+                {
+                    client.Connect(dvc.DeviceAddress, BluetoothService.SerialPort);
+                    Console.WriteLine("Connect");
+                    byte[] bytes = new byte[4];
+                    for (int i = 0; i < 4; i++)
+                    {
+                        int b = client.GetStream().ReadByte();
+                        Console.WriteLine(b);
+                        bytes[i] = (byte)b;
+                    }
+                    Console.WriteLine(String.Join("", bytes.Reverse().Select(x => (char)x).ToArray()));
+                }
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
