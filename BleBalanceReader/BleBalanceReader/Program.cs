6b5164418c53b56a40f2ed83767b42955e9ccfdb
LeiYangGH <675686066@qq.com>
2020-11-25 23:49:16 +0800

    always got same result
    Signed-off-by: LeiYangGH <675686066@qq.com>

--- BleBalanceReader/BleBalanceReader/Program.cs
+++ BleBalanceReader/BleBalanceReader/Program.cs
@@ -2,6 +2,8 @@
 using InTheHand.Net.Bluetooth;
 using InTheHand.Net.Sockets;
 using System.Linq;
+using System.Threading;
+
 namespace BleBalanceReader
 {
     class Program
@@ -9,25 +11,30 @@ namespace BleBalanceReader
 
         static void Main(string[] args)
         {
-            BluetoothClient client = new BluetoothClient();
-            var devices = client.DiscoverDevices();
-            foreach (BluetoothDeviceInfo dvc in devices)
-            {
+            BluetoothDeviceInfo foundD = null;
 
-                Console.WriteLine(dvc.DeviceName);
-                if (dvc.DeviceName == "JDY-33-SPP")
+            BluetoothClient client = new BluetoothClient();
+            if (foundD == null)
+            {
+                foundD = client.DiscoverDevices().First(x => x.DeviceName.StartsWith("JD"));
+            }
+
+            client.Connect(foundD.DeviceAddress, BluetoothService.SerialPort);
+            Console.WriteLine("Connect");
+            while (true)
+            {
+                byte[] bytes = new byte[4];
+                for (int i = 0; i < 4; i++)
                 {
-                    client.Connect(dvc.DeviceAddress, BluetoothService.SerialPort);
-                    Console.WriteLine("Connect");
-                    byte[] bytes = new byte[4];
-                    for (int i = 0; i < 4; i++)
-                    {
-                        int b = client.GetStream().ReadByte();
-                        Console.WriteLine(b);
-                        bytes[i] = (byte)b;
-                    }
-                    Console.WriteLine(String.Join("", bytes.Reverse().Select(x => (char)x).ToArray()));
+                    int b = client.GetStream().ReadByte();
+                    //Console.WriteLine(b);
+                    bytes[i] = (byte)b;
+
                 }
+                Console.WriteLine(String.Join("", bytes.Reverse().Select(x => (char)x).ToArray()));
+                //client.Close();
+                client.GetStream().Flush();
+                Thread.Sleep(1000);
             }
             Console.WriteLine("Hello World!");
         }
